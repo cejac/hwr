8af728781821870cd90185efb128dc6809dc1988
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-08-31 13:55:39 -0700

    detail page rendering

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('React is up and running!');
+import React from 'React';
+import ReactDom from 'react-dom';
+
+import Detail from './pages/detail';
+
+ReactDom.render(
+  <Detail />,
+  document.getElementById('app')
+);
