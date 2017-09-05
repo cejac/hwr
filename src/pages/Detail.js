b5c2f61058e2b4059bb37f651ce8c6da276621f6
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-09-05 14:20:05 -0700

    reconfig for new use

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,34 +1,18 @@
 import React from 'react';
-import Chance from 'chance';
 
 class Detail extends React.Component{
   constructor(props) {
     super(props);
 
-    const people = []
-
-    for(let i = 0; i < 10; i++) {
-      people.push({
-        name: chance.first(),
-        country: chance.country({full: true})
-      })
-    }
-
-    this.state = {people}
+    this.state = {commits: []};
   }
 
-  buttonClicked(){
-    const newState = {
-      name: chance.first()
-    };
 
-    this.setState(newState);
-  }
 
   render() {
       return (<div>
-      {this.state.people.map((person, index) => (
-          <p key={index}>Hello, {person.name} from {person.country}!</p>
+      {this.state.commits.map((commit, index) => (
+          <p key={index}>Some commit data here.</p>
       ))}
       </div>);
   }
