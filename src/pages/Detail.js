b46953fcf19ec433cb670f4663e94b87585e7864
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-09-07 14:34:48 -0700

    setting up to render from multiple apis

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -5,14 +5,38 @@ class Detail extends React.Component{
   constructor(props) {
     super(props);
 
-    this.state = {commits: []};
+    this.state = {
+      mode: 'commits',
+      commits: [],
+      forks: [],
+      pulls: []
+    };
   }
 
   componentWillMount() {
       ajax.get('https://api.github.com/repos/facebook/react/commits')
           .end((error, response) => {
               if (!error && response) {
-                console.dir(response.body);
+                  this.setState({ commits: response.body });
+              } else {
+                  console.log('There was an error fetching from GitHub', error);
+              }
+          }
+      );
+
+      ajax.get('https://api.github.com/repos/facebook/react/forks')
+          .end((error, response) => {
+              if (!error && response) {
+                  this.setState({ commits: response.body });
+              } else {
+                  console.log('There was an error fetching from GitHub', error);
+              }
+          }
+      );
+
+      ajax.get('https://api.github.com/repos/facebook/react/pulls')
+          .end((error, response) => {
+              if (!error && response) {
                   this.setState({ commits: response.body });
               } else {
                   console.log('There was an error fetching from GitHub', error);
@@ -20,6 +44,36 @@ class Detail extends React.Component{
           }
       );
   }
+
+  renderCommits() {
+    return this.state.commits.map((commit, index) => {
+      const author = commit.author? commit.author.login : 'Anonymous';
+
+      return(<p key={index}>
+        <strong>{author}</strong>:
+        <a href={commit.html_url}>{commit.commit.message}</a>).
+      </p>
+    })}
+
+    renderForks() {
+      return this.state.forks.map((fork, index) => {
+        const owner = fork.owner ? fork.owner.login : 'Anonymous';
+
+        return(<p key={index}>
+          <strong>{owner}</strong>: forked to
+          <a href={fork.html_url}>{fork.html_url}</a>) at {fork.create_at}.
+        </p>
+      })}
+
+      renderPulls() {
+        return this.state.pulls.map((pull, index) => {
+          const user = pull.user ? pull.user.login : 'Anonymous';
+
+          return(<p key={index}>
+            <strong>{user}</strong>:
+            <a href={pull.html_url}>{pull.body}</a>).
+          </p>
+        })}
 
 
   render() {
