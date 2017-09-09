0861b053dcc036a4bb7464199c796cb4da138836
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-09-09 14:54:39 -0700

    routing to List and Detail pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'React';
 import ReactDOM from 'react-dom';
 import {HashRouter, Switch, Route} from 'react-router-dom';
+
 import Detail from './pages/Detail';
+import List from './pages/List';
 
 const Root = () => (
   <HashRouter>
     <Switch>
-      <Route exact path='/' component={Detail} />
+      <Route exact path='/' component={List} />
+      <Route exact path='/react' component={Detail} />
     </Switch>
   </HashRouter>
 )
