e19eb4dd5065b647703958a63b39f15c426619a1
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-09-09 14:39:54 -0700

    starting to route pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'React';
 import ReactDOM from 'react-dom';
-
+import {HashRouter, Switch, Route} from 'react-router-dom';
 import Detail from './pages/Detail';
 
-ReactDOM.render(
-  <Detail message='This is coming from props!' />,
-  document.getElementById('app')
-);
+const Root = () => (
+  <HashRouter>
+    <Switch>
+      <Route exact path='/' component={Detail} />
+    </Switch>
+  </HashRouter>
+)
+
+ReactDOM.render(<Root />, document.getElementById('app'));
