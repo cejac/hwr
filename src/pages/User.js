3ec23c6ec2b5f9ebef7f55a0758bb56279fdfd26
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-09-21 14:36:27 -0700

    meh

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,42 +1,77 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ajax from 'superagent';
 
 class User extends React.Component {
-  constructor(props) {
-    super(props);
 
+  constructor(props) {
+    super();
     this.state = {
+      profile: [],
       events: []
-    };
+    }
   }
 
   componentWillMount() {
-    ajax.get(`https://api.github.com/user/${this.props.params.user}/events`)
-      .end((error, response) => {
-        if(!error && response) {
-          this.setState({events: response.body});
-        } else {
-          console.log('Error fetching user data', error);
+		const baseURL = 'https://api.github.com/users';
+    ajax.get(`${baseURL}/${this.props.match.params.username}`)
+        .end((error, response) => {
+            if (!error && response) {
+                this.setState({ profile: response.body });
+            } else {
+                console.log('Error fetching profile info, error');
+            }
         }
-      });
+    );
+    ajax.get(`${baseURL}/${this.props.match.params.username}/events`)
+        .end((error, response) => {
+            if (!error && response) {
+                this.setState({ events: response.body });
+            } else {
+                console.log('Error fetching events, error');
+            }
+        }
+    );
+	}
+
+  render(){
+    return(
+      <div>
+        <h2 className="ui header">
+          <img src={this.state.profile.avatar_url} className="ui circular image"/>
+          {this.props.match.params.username}
+        </h2>
+        <table className="ui celled table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Type</th>
+              <th>Repo</th>
+              <th>Public</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                this.state.events.map((event, index) => (
+                  <tr key={index}>
+                    <td>{event.id}</td>
+                    <td>{event.type}</td>
+                    <td>{event.repo.name}</td>
+                    <td>{event.id}</td>
+                  </tr>
+                ))
+            }
+          </tbody>
+        </table>
+        <Link to="/">
+          <button className="ui labeled icon button">
+            <i className="left chevron icon"></i>
+            Back
+          </button>
+        </Link>
+      </div>
+    )
   }
-
-  render() {
-    return(<ul>
-      {this.state.events.map((event, index) => {
-        const eventType = event.type;
-        const repoName = event.repo.name;
-        const creationDate = event.created_at;
-
-        return(<li key={index}>
-          <strong>{repoName}</strong>: {eventType}
-          at {creationDate}.
-        </li>);
-      })}
-    </ul>)
-  }
-
-
 }
 
 export default User;
